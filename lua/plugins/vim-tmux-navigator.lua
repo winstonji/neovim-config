e78c4ace21ee8fc580bd20a45ea3a9a8d1e0eed9
Winston Ji <winston.wy.ji@gmail.com>
2025-03-10 21:39:12 -0600

    new kickstart config

--- lua/plugins/vim-tmux-navigator.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "christoomey/vim-tmux-navigator",
-    config = function()
-        vim.keymap.set("n", "<Tab-h>", ":TmuxNavigateLeft<CR>")
-        vim.keymap.set("n", "<Tab-j>", ":TmuxNavigateDown<CR>")
-        vim.keymap.set("n", "<Tab-k>", ":TmuxNavigateUp<CR>")
-        vim.keymap.set("n", "<Tab-l>", ":TmuxNavigateRight<CR>")
-    end,
-}
